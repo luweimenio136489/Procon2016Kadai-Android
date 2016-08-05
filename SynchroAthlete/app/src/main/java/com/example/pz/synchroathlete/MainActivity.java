9ba245029a4a25751ea6ce3938dee7bc7ca8fcfd
Akama Kousei <akakou571@outlook.jp>
2016-08-05 18:22:30 +0900

    Android6以上の対応、パーミッションの追加

--- SynchroAthlete/app/src/main/java/com/example/pz/synchroathlete/MainActivity.java
+++ SynchroAthlete/app/src/main/java/com/example/pz/synchroathlete/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.pz.synchroathlete;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -31,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //パーミッションの許諾
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
 
         // ボタンを設定
         Button buttonA = (Button)findViewById(R.id.buttonA);
@@ -73,23 +80,34 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
-        if (buttonB != null) {
-            buttonB.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    FileOutputStream fos = null;
-                    try {
-                        fos = new FileOutputStream(file,true);
-                        fos.write("1,2,3,4,5,6\r\n".getBytes());
-                        fos.close();
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(file,true);
+                    fos.write("1,2,3,4,5,6\r\n".getBytes());
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            });
+            }
+        });
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        if (requestCode == 100) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+                //拒否された時
+            }
         }
     }
+
 }
 
