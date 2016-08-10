507266e2bbbc0c1e1bfcead5bc2049640f734b61
khrom <devs@outlook.jp>
2016-08-10 15:26:24 +0900

    fix: UIでresult code が表示できるようにした

--- Theta_S_Remote/app/src/main/java/nittcprocon/thetasremote/MainActivity.java
+++ Theta_S_Remote/app/src/main/java/nittcprocon/thetasremote/MainActivity.java
@@ -1,10 +1,14 @@
 package nittcprocon.thetasremote;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.os.AsyncTask;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
@@ -51,89 +55,114 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-}
 
-class ThetaS_Shutter {
 
-    /**
-     * セッションを開始。セッションIDを発行する。
-     */
-    void connect() {
-        sendRequest("{\"name\": \"camera.startSession\" ,\"parameters\": {}}");
+    public void sendRequest(String args) {
+        SendRequestAsync asyncTask = new SendRequestAsync(getApplicationContext(), args);
+        asyncTask.execute();
     }
 
-    /**
-     * 静止画撮影を開始する。
-     */
-    void shutter() {
-        sendRequest("{\"name\": \"camera.takePicture\" ,\"parameters\": {\"sessionId\" :\"SID_0001\"}}");
-    }
 
-    /**
-     * v2.0
-     * 連続撮影を開始する。
-     */
-    void startcapture() {
-        sendRequest("{\"name\": \"camera._startCapture\" ,\"parameters\": {\"sessionId\" :\"SID_0001\"}}");
-    }
+    public class SendRequestAsync extends AsyncTask<Void, Void, String> {
+        String payload_;
+        Context context_;
 
-    /**
-     * v2.0
-     * 連続撮影を停止する。
-     */
-    void stopcapture() {
-        sendRequest("{\"name\": \"camera._stopCapture\" ,\"parameters\": {\"sessionId\" :\"SID_0001\"}}");
-    }
-
-    /**
-     * 撮影モード指定
-     *
-     * @param mode true:動画 false:画像
-     */
-    void mode(boolean mode) {
-        String value;
-        if (mode == true) {
-            value = "_video";
-        } else {
-            value = "image";
+        public SendRequestAsync(Context context, String payload) {
+            context_ = context;
+            payload_ = payload;
         }
 
-        option("captureMode", value);
+        @Override
+        protected String doInBackground(Void... params) {
+            String result = null;
+
+            // リクエストボディを作る
+            RequestBody requestBody = RequestBody.create(
+                    MediaType.parse("application/json"), payload_
+            );
+
+            // リクエストオブジェクトを作って
+            Request request = new Request.Builder()
+                    .url("http://192.168.1.1/osc/commands/execute")
+                    .post(requestBody)
+                    .build();
+            // クライアントオブジェクトを作って
+            OkHttpClient client = new OkHttpClient();
+            // リクエストして結果を受け取って
+            try {
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.getMessage();
+            }
+            // 返す
+//                return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(context_, s, Toast.LENGTH_SHORT).show();
+            Log.i("result code ", "CODE:" + s);
+        }
+
     }
 
-    public static void option(String option_name, String option_value) {
-        sendRequest("{\"name\": \"camera.setOptions\" ,\"parameters\": {\"sessionId\" :\"SID_0001\", \"options\": {\"" + option_name + "\": \"" + option_value + "\"}}}");
-    }
+    public class ThetaS_Shutter {
 
-    public static void sendRequest(final String payload) {
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-                String result = null;
+        /**
+         * セッションを開始。セッションIDを発行する。
+         */
+        void connect() {
+            sendRequest("{\"name\": \"camera.startSession\" ,\"parameters\": {}}");
+        }
 
-                // リクエストボディを作る
-                RequestBody requestBody = RequestBody.create(
-                        MediaType.parse("application/json"), payload
-                );
+        /**
+         * 静止画撮影を開始する。
+         */
+        void shutter() {
+            sendRequest("{\"name\": \"camera.takePicture\" ,\"parameters\": {\"sessionId\" :\"SID_0001\"}}");
+        }
+        
 
-                // リクエストオブジェクトを作って
-                Request request = new Request.Builder()
-                        .url("http://192.168.1.1/osc/commands/execute")
-                        .post(requestBody)
-                        .build();
-                // クライアントオブジェクトを作って
-                OkHttpClient client = new OkHttpClient();
-                // リクエストして結果を受け取って
-                try {
-                    Response response = client.newCall(request).execute();
-                    result = response.body().string();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // 返す
-                return result;
+        /**
+         * v2.0
+         * 連続撮影を開始する。
+         */
+        void startcapture() {
+            sendRequest("{\"name\": \"camera._startCapture\" ,\"parameters\": {\"sessionId\" :\"SID_0001\"}}");
+        }
+
+        /**
+         * v2.0
+         * 連続撮影を停止する。
+         */
+        void stopcapture() {
+            sendRequest("{\"name\": \"camera._stopCapture\" ,\"parameters\": {\"sessionId\" :\"SID_0001\"}}");
+        }
+
+        /**
+         * 撮影モード指定
+         *
+         * @param mode true:動画 false:画像
+         */
+        void mode(boolean mode) {
+            String value;
+            if (mode == true) {
+                value = "_video";
+            } else {
+                value = "image";
             }
 
-        }.execute();
+            option("captureMode", value);
+        }
+
+        private void option(String option_name, String option_value) {
+            sendRequest("{\"name\": \"camera.setOptions\" ,\"parameters\": {\"sessionId\" :\"SID_0001\", \"options\": {\"" + option_name + "\": \"" + option_value + "\"}}}");
+        }
+
+
     }
+
 }
