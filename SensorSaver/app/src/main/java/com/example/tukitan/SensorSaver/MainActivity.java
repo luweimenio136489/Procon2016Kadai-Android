f2252c8db403231cbf6f059a0e0cc108a7ab521d
komaki <kosen.komaki@gmail.com>
2016-08-14 10:45:42 +0900

    ThetaSとの接続完了

--- SensorSaver/app/src/main/java/com/example/tukitan/SensorSaver/MainActivity.java
+++ SensorSaver/app/src/main/java/com/example/tukitan/SensorSaver/MainActivity.java
@@ -8,10 +8,13 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +25,14 @@ import android.widget.Toast;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -42,6 +53,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private static boolean whiteState = false;
     long startTime;
     TextView sValue,aValue;
+    private LogView logView; //ログ
+    private static String settionID = "SID_0000"; //http通信側のsessionID
+    public static final long SLEEP_TIME_SECONDS = 120; ///sessionIDの更新間隔
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +66,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         // ボタンを設定
         ((ImageButton) findViewById(R.id.startButton)).setOnClickListener(this);
-        ((Button) findViewById(R.id.initButton)).setOnClickListener(this);
         ((ImageButton) findViewById(R.id.stopButton)).setOnClickListener(this);
+        ((Button) findViewById(R.id.initButton)).setOnClickListener(this);
+        ((Button) findViewById(R.id.reconnect)).setOnClickListener(this);
 
         sValue = (TextView) findViewById(R.id.sensorValue);
         aValue = (TextView) findViewById(R.id.attitudeVale);
@@ -62,22 +77,74 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         file = new File(Environment.getExternalStorageDirectory() + "/SynchroAthlete/test.txt");
         dataFile = new File(Environment.getExternalStorageDirectory() + "/SynchroAthlete/sensorData.txt");
         file.getParentFile().mkdir();
-        try {
-            //最初の一行目に"write hedder infomations here"と書く
-            FileOutputStream fos = new FileOutputStream(file);
-            FileOutputStream fos2 = new FileOutputStream(dataFile);
-            fos.write("write hedder infomations here.\r\n".getBytes());
-            fos2.write("Writing Sensor Data.\r\n".getBytes());
-            fos.close();
-            fos2.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        LogFragment logFragment = (LogFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.log_fragment);
+        logView = logFragment.getLogView();
+        autoRefreshSettion();
 
     }
+    /**
+     * セッションの自動更新
+     * SLEEP_TIME_SESSIONで秒設定
+     */
+    public void autoRefreshSettion() {
+        refleshSession();
+        final Handler handler = new Handler();
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    // マルチスレッドにしたい処理 ここから
+                    try {
+                        Thread.sleep(1000 * SLEEP_TIME_SECONDS);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            refleshSession();
+                        }
+                    });
+                }
+                // マルチスレッドにしたい処理 ここまで
+            }
+        }).start();
+    }
+    /**
+     * リクエスト送信
+     *
+     * @param args
+     */
+    public void sendRequest(String args) {
+        SendRequestAsync asyncTask = new SendRequestAsync(getApplicationContext(), args);
+        asyncTask.execute();
+
+    }
+    /**
+     * セッションのリフレッシュ
+     */
+    public void refleshSession() {
+        //logHTML("<font color=\"Green\">" + ThetaRequest.getConnectRequest() + "</font>", false);
+        sendRequest(ThetaRequest.getConnectRequest());
+    }
+    /**
+     * htmlタグを使ってログを履く
+     *
+     * @param text
+     * @param isclear
+     */
+    public void logHTML(String text, boolean isclear) {
+
+        if (isclear) {
+            logView.setText("");
+            logView.append(Html.fromHtml("<br>" + text));
+        } else {
+            logView.append(Html.fromHtml("<br>" + text));
+        }
+    }
     protected void onResume() {
         super.onResume();
 
@@ -141,21 +208,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             case R.id.startButton:
                 if (isInited) {
                     whiteState = true;
+                    writeFileInit();
                     startTime = System.currentTimeMillis();
                     (new Thread(new inputData())).start();
                     Toast.makeText(this, "書き込みを開始しました", Toast.LENGTH_SHORT).show();
+                    sendRequest(ThetaRequest.getStartcaptureRequest(settionID));
                     isInited = false;
                 } else Toast.makeText(this, "先に初期化をしてください", Toast.LENGTH_SHORT).show();
                 break;
+
             case R.id.initButton:
                 Toast.makeText(this, "初期化しました", Toast.LENGTH_SHORT).show();
+                logHTML(ThetaRequest.getModeRequest(true, settionID), false);
+                sendRequest(ThetaRequest.getModeRequest(true, settionID));
                 isInited = true;
                 break;
-            case R.id.stopButton:
 
+            case R.id.stopButton:
                 whiteState = false;
                 Toast.makeText(this, "書き込みを停止しました", Toast.LENGTH_SHORT).show();
+                sendRequest(ThetaRequest.getStopcaptureRequest(settionID));
                 break;
+
+            case R.id.reconnect:
+                refleshSession();
+                break;
+
         }
     }
 
@@ -165,9 +243,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 + accel[0] + "," + accel[1] + "," + accel[2]
                 + magnetic[0] + "," + magnetic[1] + "," + magnetic[2]
                 + gyro[0] + "," + gyro[1] + "," + gyro[2] + "\r\n";
+
         String outputAttitude = System.currentTimeMillis() -startTime
                 + attitude[0] * Rad2Dec + "," + attitude[1] * Rad2Dec
                 + gravity;
+
         FileOutputStream fos2 = null;
         try {
             fos2 = new FileOutputStream(dataFile, true);
@@ -196,7 +276,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         attitude[1] = Math.asin(accelData[0]/9.81);
         return attitude;
     }
-    // データを入力するスレッド
+
+    private void writeFileInit(){
+        try {
+            //最初の一行目に"write hedder infomations here"と書く
+            /*
+             * fos:傾きを記録するファイル
+             * fos2:9軸センサのデータをそのまま保存するファイル
+             */
+            FileOutputStream fos = new FileOutputStream(file);
+            FileOutputStream fos2 = new FileOutputStream(dataFile);
+            fos.write("write hedder infomations here.\r\n".getBytes());
+            fos2.write("Writing Sensor Data.\r\n".getBytes());
+            fos.close();
+            fos2.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // データを出力するスレッド
     private class inputData implements Runnable {
         @Override
         public void run() {
@@ -208,6 +309,100 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     System.out.println(e);
                 }
             }
+        }
+    }
+    /**
+     * HTTP通信を行うクラス
+     */
+    public class SendRequestAsync extends AsyncTask<Void, Void, String> {
+        String payload_;
+        Context context_;
+
+        public SendRequestAsync(Context context, String payload) {
+            context_ = context;
+            payload_ = payload;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String result = null;
+
+            // リクエストボディを作る
+            RequestBody requestBody = RequestBody.create(
+                    MediaType.parse("application/json"), payload_
+            );
+
+            // リクエストオブジェクトを作って
+            Request request = new Request.Builder()
+                    .url("http://192.168.1.1/osc/commands/execute")
+                    .post(requestBody)
+                    .build();
+            // クライアントオブジェクトを作って
+            OkHttpClient client = new OkHttpClient();
+            // リクエストして結果を受け取って
+            try {
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                return "CATCH ERROR::" + e.getMessage();
+            }
+            // 返す
+//                return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                if (isSessionId(s)) {
+                    JSONObject jsonObject = new JSONObject(s);
+                    logHTML("<font color=\"Green\">" + s + "</font>", false);
+                    settionID = jsonObject.getJSONObject("results").getString("sessionId");
+                    logHTML("セッション更新", false);
+                } else if (isInvalidSessionId(s)) {
+                    logHTML("<font color=\"Red\">" + s + "</font>", false);
+                    logHTML("<font color=\"Red\">" + "セッションの更新します" + "</font>", false);
+                    refleshSession();
+                } else if (s.contains("CATCH ERROR")) {
+                    logHTML("<font color=\"Red\">" + s + "</font>", false);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * 無効なセッションIDが含まれているか
+         *
+         * @param data
+         * @return
+         */
+        public boolean isInvalidSessionId(String data) {
+            try {
+                JSONObject jsonObject = new JSONObject(data);
+                if (jsonObject.getJSONObject("error").getString("code") == "invalidSessionId")
+                    return true;
+            } catch (JSONException e) {
+                return false;
+            }
+            return false;
+        }
+
+        /**
+         * セッションIDが含まれているかどうか
+         *
+         * @param data
+         * @return
+         */
+        public boolean isSessionId(String data) {
+            try {
+                JSONObject jsonObject = new JSONObject(data);
+                if (jsonObject.getJSONObject("results").getString("sessionId") != null)
+                    return true;
+            } catch (JSONException e) {
+                return false;
+            }
+            return false;
         }
     }
 }
