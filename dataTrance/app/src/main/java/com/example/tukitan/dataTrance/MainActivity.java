cea44f4c041247eb7ad4d34c6bba2a1cb5b6a55c
komaki <kosen.komaki@gmail.com>
2016-09-23 02:53:38 +0900

    add: アプリ起動時はスリープモードにならないようにした

--- dataTrance/app/src/main/java/com/example/tukitan/dataTrance/MainActivity.java
+++ dataTrance/app/src/main/java/com/example/tukitan/dataTrance/MainActivity.java
@@ -19,6 +19,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -73,6 +74,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         state = (TextView) findViewById(R.id.genzai);
         counter = (EditText) findViewById(R.id.countTimer);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
     }
 
